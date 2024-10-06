338da5c8eef90bac7cb4d18a98ab664378c5c1e5
Daniel VM <157084240+Agata6391@users.noreply.github.com>
2024-10-06 16:46:15 -0600

    update user dashboard
    incremento de de libros y retiro de libros

--- boo-books/src/components/Crud.js
+++ boo-books/src/components/Crud.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+
 import './Crud.css'; 
 import AdminLibros from "./adminLibros";
 import AdminUsers from "./adminUsers";
--- boo-books/src/components/adminLibros.js
+++ boo-books/src/components/adminLibros.js
@@ -1,39 +1,52 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
- 
 
 const AdminLibros = () => {
   const [libros, setLibros] = useState([]);
-  const [editingLibro, setEditingLibro] = useState(null);  // Controla el libro que se está editando
-  const [showCreateForm, setShowCreateForm] = useState(false);  // Controla si se muestra el formulario de creación
+  const [editingLibro, setEditingLibro] = useState(null); // Controla el libro que se está editando
+  const [showCreateForm, setShowCreateForm] = useState(false); // Controla si se muestra el formulario de creación
   const [formValues, setFormValues] = useState({
-    nombre: '',
-    autor: '',
-    genero: ''
+    nombre: "",
+    autor: "",
+    isbn: "",
+    imagen: "",
+    publicacion: "",
+    cantidadDisponible: "",
+    genero: "",
   });
 
   // Obtener todos los libros al cargar el componente
   useEffect(() => {
-    axios.get("/api/libros")
+    axios
+      .get("/api/libros")
       .then((response) => setLibros(response.data))
       .catch((error) => console.error(error));
   }, []);
 
-  // Eliminar un libro
+  // Eliminar un libro (solo admin)
   const deleteLibro = (id) => {
-    axios.delete(`/api/libros/${id}`)
+    axios
+      .delete(`/api/libros/${id}`)
       .then(() => setLibros(libros.filter((libro) => libro._id !== id)))
       .catch((error) => console.error(error));
   };
 
-  // Manejar el inicio de la edición
+  // Manejar el inicio de la edición (solo admin)
   const startEdit = (libro) => {
     setEditingLibro(libro._id);
-    setFormValues({ nombre: libro.nombre, autor: libro.autor, genero: libro.genero });
-    setShowCreateForm(false);  // Asegurarnos de que no se muestre el formulario de creación
+    setFormValues({
+      nombre: libro.nombre,
+      autor: libro.autor,
+      isbn: libro.isbn,
+      imagen: libro.imagen,
+      publicacion: libro.publicacion,
+      cantidadDisponible: libro.cantidadDisponible,
+      genero: libro.genero,
+    });
+    setShowCreateForm(false); // Asegurarnos de que no se muestre el formulario de creación
   };
 
-  // Manejar los cambios en el formulario
+  // Manejar los cambios en el formulario (solo admin)
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
@@ -41,22 +54,42 @@ const AdminLibros = () => {
 
   // Actualizar un libro
   const updateLibro = (id) => {
-    axios.put(`/api/libros/${id}`, formValues)
+    axios
+      .put(`/api/libros/${id}`, formValues)
       .then((response) => {
-        setLibros(libros.map((libro) => (libro._id === id ? response.data : libro)));
-        setEditingLibro(null);  // Salir del modo de edición
-        setFormValues({ nombre: '', autor: '', genero: '' });  // Resetear los valores del formulario
+        setLibros(
+          libros.map((libro) => (libro._id === id ? response.data : libro))
+        );
+        setEditingLibro(null); // aqui se sale de editar
+        setFormValues({
+          nombre: "",
+          autor: "",
+          isbn: "",
+          imagen: "",
+          publicacion: "",
+          cantidadDisponible: "",
+          genero: "",
+        }); // Resetear los valores del formulario
       })
       .catch((error) => console.error(error));
   };
 
   // Crear un nuevo libro
   const createLibro = () => {
-    axios.post('/api/libros', formValues)
+    axios
+      .post("/api/libros", formValues)
       .then((response) => {
         setLibros([...libros, response.data]);
-        setShowCreateForm(false);  // Ocultar el formulario después de crear el libro
-        setFormValues({ nombre: '', autor: '', genero: '' });  // Resetear los valores del formulario
+        setShowCreateForm(false); // Ocultar el formulario después de crear el libro
+        setFormValues({
+          nombre: "",
+          autor: "",
+          isbn: "",
+          imagen: "",
+          publicacion: "",
+          cantidadDisponible: "",
+          genero: "",
+        }); // Resetear los valores del formulario
       })
       .catch((error) => console.error(error));
   };
@@ -67,7 +100,7 @@ const AdminLibros = () => {
 
       {/* Botón para mostrar el formulario de creación */}
       <button onClick={() => setShowCreateForm(!showCreateForm)}>
-        {showCreateForm ? 'Cancelar' : 'Crear Nuevo Libro'}
+        {showCreateForm ? "Cancelar" : "Crear Nuevo Libro"}
       </button>
 
       {/* Formulario de creación de libro */}
@@ -90,6 +123,34 @@ const AdminLibros = () => {
           />
           <input
             type="text"
+            name="isbn"
+            placeholder="ISBN"
+            value={formValues.isbn}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="imagen"
+            placeholder="URL de la Imagen"
+            value={formValues.imagen}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="publicacion"
+            placeholder="Fecha de Publicación"
+            value={formValues.publicacion}
+            onChange={handleInputChange}
+          />
+          <input
+            type="number"
+            name="cantidadDisponible"
+            placeholder="Cantidad Disponible"
+            value={formValues.cantidadDisponible}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
             name="genero"
             placeholder="Género"
             value={formValues.genero}
@@ -103,7 +164,7 @@ const AdminLibros = () => {
         {libros.map((libro) => (
           <li key={libro._id}>
             {editingLibro === libro._id ? (
-              <div >
+              <div>
                 <input
                   type="text"
                   name="nombre"
@@ -118,19 +179,47 @@ const AdminLibros = () => {
                 />
                 <input
                   type="text"
+                  name="isbn"
+                  value={formValues.isbn}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  name="imagen"
+                  value={formValues.imagen}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  name="publicacion"
+                  value={formValues.publicacion}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="number"
+                  name="cantidadDisponible"
+                  value={formValues.cantidadDisponible}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
                   name="genero"
                   value={formValues.genero}
                   onChange={handleInputChange}
                 />
-                <button onClick={() => updateLibro(libro._id)}>Guardar Cambios</button>
+                <button onClick={() => updateLibro(libro._id)}>
+                  Guardar Cambios
+                </button>
                 <button onClick={() => setEditingLibro(null)}>Cancelar</button>
               </div>
             ) : (
               <div className="bookList">
                 {libro.nombre} - {libro.autor} ({libro.genero})
                 <div className="buttons">
-                <button onClick={() => deleteLibro(libro._id)}>Eliminar</button>
-                <button onClick={() => startEdit(libro)}>Editar</button>
+                  <button onClick={() => deleteLibro(libro._id)}>
+                    Eliminar
+                  </button>
+                  <button onClick={() => startEdit(libro)}>Editar</button>
                 </div>
               </div>
             )}
